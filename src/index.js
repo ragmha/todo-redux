45c511497d98ad2b932657664ad467bc6fc9c411
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-17 14:17:19 +0300

    add redux store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 
-const state = {
-  todos: [
-    { id: 1, name: 'Render static UI', isComplete: true },
-    { id: 2, name: 'Create initial state', isComplete: true },
-    { id: 3, name: 'Render based on state', isComplete: false },
-  ],
+const render = () => {
+  const state = store.getState();
+  ReactDOM.render(<App {...state} />, document.getElementById('root'));
 };
 
-ReactDOM.render(<App todos={state.todos} />, document.getElementById('root'));
+render();
 registerServiceWorker();
