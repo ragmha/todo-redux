f497d740a85d04ef0cccd168235b3b2972bdba0b
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-29 16:23:27 +0300

    respond to a single redux action

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -7,8 +7,8 @@ const initState = {
 };
 
 const CURRENT_UPDATE = 'CURRENT_UPDATE';
-const TODO_ADD = 'TODO_ADD';
-const TODOS_LOAD = 'TODOS_LOAD';
+export const TODO_ADD = 'TODO_ADD';
+export const TODOS_LOAD = 'TODOS_LOAD';
 
 export const updateCurrent = val => ({ type: CURRENT_UPDATE, payload: val });
 export const loadTodos = todos => ({ type: TODOS_LOAD, payload: todos });
@@ -16,6 +16,7 @@ export const addTodo = todo => ({ type: TODO_ADD, payload: todo });
 
 export const fetchTodos = () => {
   return dispatch => {
+    dispatch(showMessage('Loading Todos'));
     getTodos().then(todos => dispatch(loadTodos(todos)));
   };
 };
