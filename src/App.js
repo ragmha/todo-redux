8e0fef16c8c28643567aad2e3b3cf9d2f6748e36
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-29 16:51:24 +0300

    filter state data with selecter function and react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,7 +20,10 @@ class App extends Component {
           <div className="Todo-App">
             <Message message="Hello There" />
             <TodoForm />
-            <TodoList />
+            <Route
+              path="/:filter?"
+              render={({ match }) => <TodoList filter={match.params.filter} />}
+            />
             <Footer />
           </div>
         </Router>
